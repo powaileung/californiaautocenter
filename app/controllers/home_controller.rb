a8c210233a0650bdab3c08618488921ea5a9aae2
Powai Leung <pwleung1489@gmail.com>
2019-11-17 07:12:55 +0000

    input cac wireframing

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about_us
+  end
+
+  def coupons
+  end
+
+  def location
+  end
+
+  def contact_us
+  end
+
+  def term
+  end
+
+  def privacy
+  end
+
+  def career
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get 'about_us', to: 'home#about_us'
+  get 'coupons', to: 'home#coupons'
+  get 'location', to: 'home#location'
+  get 'contact_us', to: 'home#contact_us'
+  get 'term', to: 'home#term'
+  get 'privacy', to: 'home#privacy'
+  get 'career', to: 'home#career'
+  get 'isabel2jade', to: 'home#isabel2jade'
 end

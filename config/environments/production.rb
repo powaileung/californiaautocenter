b2562a001c3308b22b4dd58c3f93358f94ab47fd
Powai Leung <pwleung1489@gmail.com>
2019-12-19 05:15:27 +0000

    input sendgrid production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,18 @@ Rails.application.configure do
   # create custom error pages procedures.
   config.exceptions_app = self.routes 
 
-  config.action_mailer.default_url_options = { host: 'cac-powai-leung.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://cac-powai-leung.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
